2dcac840fd5a7666466af841cbeea49f144d31bc
Miguel Luis Y. Sanchez <miguellysanchez@gmail.com>
2018-08-14 12:12:17 +0800

    Added example for actor coroutine

--- app/src/main/java/com/sample/mycoroutines/p6_channels/ChannelsActivity.kt
+++ app/src/main/java/com/sample/mycoroutines/p6_channels/ChannelsActivity.kt
@@ -6,11 +6,16 @@ import android.view.View
 import com.sample.mycoroutines.R
 import kotlinx.android.synthetic.main.activity_channels.*
 import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.CoroutineName
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.channels.Channel
+import kotlinx.coroutines.experimental.channels.actor
+import kotlinx.coroutines.experimental.channels.consumeEach
+import kotlinx.coroutines.experimental.channels.produce
 import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
 import java.util.Random
+import kotlin.coroutines.experimental.coroutineContext
 
 class ChannelsActivity : AppCompatActivity() {
 
@@ -26,6 +31,12 @@ class ChannelsActivity : AppCompatActivity() {
         channels_button_run_simple_channels.setOnClickListener {
             simpleChannels()
         }
+        channels_button_run_producer_consumer.setOnClickListener {
+            producerConsumer()
+        }
+        channels_button_run_actor.setOnClickListener {
+            doActorDemo()
+        }
     }
 
     private fun simpleChannels() {
@@ -36,24 +47,83 @@ class ChannelsActivity : AppCompatActivity() {
                 val x = random.nextInt(1000)
                 println("Sending value [$x]>>>")
                 channel.send(x)
-                delay(1100)
+                delay(10)
             }
             println("Channel Closed for sending ")
         }
-        launch(UI) {
+        launch(UI + CoroutineName("ABC")) {
             var numExpected = 10
             while (numExpected > 0) {
                 numExpected--
-                println("-->Receiving")
-                channels_textview_indicator.text = ">>>Received value: ${channel.receive()}"
-                println("-->Received")
+//                println("-->ABC Receiving")
+                val received = channel.receive()
+                channels_textview_indicator.text = ">>>Received value @${coroutineContext[CoroutineName]}: $received}"
+                println("-->ABC Received: [$received]")
 
-                delay(500)
+                delay(750)
             }
-            println("Done receiving!")
-            channel.close()
+            println("ABC Done receiving!")
             channels_button_run_simple_channels.visibility = View.VISIBLE
         }
 
+        launch(UI + CoroutineName("XYZ")) {
+            var numExpected = 10
+            while (numExpected > 0) {
+                numExpected--
+//                println("-->XYZ Receiving")
+                val received = channel.receive()
+                channels_textview_indicator.text = ">>>Received value @${coroutineContext[CoroutineName]}: $received"
+                println("-->XYZ Received [$received]")
+                delay(1200)
+            }
+            println("XYZ Done receiving, closing channel!")
+            channel.close()
+            channels_button_run_simple_channels.visibility = View.VISIBLE
+        }
     }
+
+    private fun producerConsumer() {
+        val intProducer = produce<Int> {
+            for (x in 1..5) {
+                send(random.nextInt(9999))
+            }
+        }
+        launch(UI) {
+            intProducer.consumeEach {
+                channels_textview_indicator.text = "Gaining $it HP"
+                delay(1000)
+            }
+            println("Finished consumption")
+        }
+    }
+
+
+    private fun doActorDemo() {
+        val chuckNorris = actor<Int>(UI) {
+            print(">>>Chuck Norris is doing ")
+//            println("Received 1 ${channel.receive()}")
+//            println("Received 2 ${channel.receive()}")
+//            println("Received 3 ${channel.receive()}")
+//            println("Received 4 ${channel.receive()}")
+            for (action in channel) {
+                val actionPerformed = when (action % 4) {
+                    0 -> "a Backflip Kick,"
+                    1 -> "a Roundhouse Kick,"
+                    2 -> "a Double Roundhouse Kick,"
+                    else -> "a Whirlwind Roundhouse Kick,"
+                }
+                print("$actionPerformed")
+            }
+            println("...and standing still, all at the same time!")
+        }
+        launch {
+            chuckNorris.send(random.nextInt())
+            chuckNorris.send(random.nextInt())
+            chuckNorris.send(random.nextInt())
+            chuckNorris.send(random.nextInt())
+            chuckNorris.close()
+        }
+    }
+
+
 }
